2dfdca413e94da294fe9eec079336389de3df644
Zach Gulde <zachgulde@gmail.com>
2017-08-15 09:26:29 -0500

    Prompt users for color preference

--- js/external.js
+++ js/external.js
@@ -1,3 +1,9 @@
 'use strict';
 
 console.log('Hello from external js!');
+
+alert('Welcome to my website!');
+
+var usersFavoriteColor = prompt('What is your favorite color?');
+
+alert('Yay!, ' + usersFavoriteColor + ' is my favorite too!');
