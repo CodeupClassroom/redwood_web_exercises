660ee6ae1ef19372916b98514c39dca3124f2c7b
Luis Montealegre <montealegreluis@gmail.com>
2017-08-16 10:25:45 -0500

    Add comments explaining the differences between
    * statements
    * expressions
    * composite expressions

--- js/switch.js
+++ js/switch.js
@@ -4,15 +4,24 @@
 // This creates two variables:
 //     one with the colors of the rainbow, and another with a single randome
 //     another with a single random color value
-var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
-var color = colors[Math.floor(Math.random()*colors.length)];
+// 1. Variable declaration. 2. Assignment
+var colors;  // Not an expression
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']; // It is an expression
+// 1. Variable declaration. 2. Assignment
+// arithmetic operation is an expression
+// a function call is an expression IF it return a value
+var color = colors[Math.floor(Math.random()*colors.length)]; //NOT an expression
 
-console.log(color);
+console.log(color); // Not an expression
 
-switch (color) {
-    case "red":
+// Sequential statements
+
+// Compound statement, or composite statement
+
+switch (color) {  // 1 statement, not an expression
+    case "red": // start of the case block
         console.log("color red");
-        break;
+        break; // end of the case block
     case "orange":
         console.log("color orange");
         break;
@@ -27,21 +36,23 @@ switch (color) {
         break;
     default:
         console.log("I do not know anything by that color");
+} // end of block for switch statement
 
-    // TODO: create a case statement that will handle every color except indigo and violet
-    // TODO: when a color is encountered log a message that tells the color, and an object of that color
-    //       example: Blue is the color of the sky.
+var number;
+// to represent values
+number = 1 > 2 /* any boolean expression*/ ? /* any expression*/ 3 : 4 /* any expression */;
+// x = (boolean expression) ? value a : value b;
 
-    // TODO: create a default case that will catch indigo and violet
-    // TODO: for the default case, log: I do not know anything by that color.
+// to represent procedures
+if (1 > 2 /* any boolean expression */) { // consequence 1 o r many statements
+    //I get the discount
+    //Finish the rest of the functionality to give discounts
+    number = 3;
+} else { // alternative, 1 or many statements
+    number = 4;
 }
 
 // Cameron case
-var sale = 180;
+var sale;
+sale = 180;
 var minSale = 200;
-
-if(sale > minSale){
-    //I get the discount
-    //Finish the rest of the functionality to give discounts
-
-}
