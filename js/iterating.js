1f264c970ec5460ce7d52f4de7c5e459ff135959
Fernando <fernandoza@gmail.com>
2017-08-18 14:59:21 -0500

    Finish the solution for the arrays 1st exercise.

---
+++ js/iterating.js
@@ -0,0 +1,18 @@
+(function(){
+    "use strict";
+
+    // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
+     var names = new Array("Fer", "Luis", "Ryan", "Zach");
+
+    // TODO: Create a log statement that will log the number of elements in the names array.
+
+    console.log(names.length);
+
+    // TODO: Create log statements that will print each of the names array elements individually.
+
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
+
+})();
