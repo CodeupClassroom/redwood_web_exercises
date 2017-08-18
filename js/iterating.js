c5f7fc70bf0b3be4e76a1e7f79fc1a08e5efe0e4
Fernando <fernandoza@gmail.com>
2017-08-18 16:12:50 -0500

    finish the iterating solution

--- js/iterating.js
+++ js/iterating.js
@@ -2,7 +2,7 @@
     "use strict";
 
     // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
-     var names = new Array("Fer", "Luis", "Ryan", "Zach");
+     var names = new Array("Fer", "Luis", "Ryan", "Zach", "Justin");
 
     // TODO: Create a log statement that will log the number of elements in the names array.
 
@@ -10,9 +10,14 @@
 
     // TODO: Create log statements that will print each of the names array elements individually.
 
-    console.log(names[0]);
-    console.log(names[1]);
-    console.log(names[2]);
-    console.log(names[3]);
+    for(var x = 0; x < names.length; x++){
+        console.log(names[x]);
+    }
+
+    var printNmaes = function (element){
+        console.log(element);
+    }
+
+    names.forEach( printNmaes );
 
 })();
