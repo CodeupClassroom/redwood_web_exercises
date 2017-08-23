4bf6e208ae3d7db1ba54ec39efb6dbba6c886161
Luis Montealegre <montealegreluis@gmail.com>
2017-08-23 09:15:59 -0500

    Complete the objects exercise

--- js/objects.js
+++ js/objects.js
@@ -45,4 +45,37 @@
             + ", the total to pay after the discount is $" + totalAfterDiscount
         ;
     }
+
+    var books = [
+        {
+            title: "97 Things Every Programmer Should Know",
+            author: {firstName: "Kevlin", lastName: "Henney"}
+        },
+        {
+            title: "Growing Object-Oriented Software Guided by Tests",
+            author: {firstName: "Steve", lastName: "Freeman"}
+        },
+        {
+            title: " Working Effectively with Legacy Code",
+            author: {firstName: "Michael", lastName:  "Feathers"}
+        },
+        {
+            title: "Refactoring",
+            author: {firstName: "Martin", lastName: "Fowler"}
+        },
+        {
+            title: "The Pragmatic Programmer",
+            author: { firstName: "Andy", lastName: "Hunt"}
+        }
+    ];
+
+    console.log(books);
+
+    books.forEach(function (book, i) {
+        console.log("Book #" + (i + 1));
+        console.log("Title: " + book.title);
+        console.log("Author: " + book.author.firstName + " " + book.author.lastName);
+        console.log("---");
+    });
+
 })();
